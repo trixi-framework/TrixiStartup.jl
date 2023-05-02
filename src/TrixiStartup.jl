f6aa41a32633d57c6d9f73dd9e13d603b4e13ac0
Michael Schlottke-Lakemper <michael@sloede.com>
2023-05-02 21:32:53 +0200

    Replace `trixi_include`s with corresponding `include`s

--- src/TrixiStartup.jl
+++ src/TrixiStartup.jl
@@ -8,68 +8,28 @@ using Reexport: @reexport
 
 @compile_workload begin
   # Compressible Euler, P4estMesh, 2D, polydeg=3
-  trixi_include(@__MODULE__,
-                joinpath("..", "precompile", "p4est_2d_dgsem_elixir_euler_shockcapturing_ec.jl"),
-                tspan=(0.0, 0.01),
-                initial_refinement_level=0,
-                polydeg=3,
-               )
+  include(joinpath("..", "precompile", "p4est_2d_dgsem_elixir_euler_shockcapturing_ec_polydeg3.jl"))
 
   # Compressible Euler, P4estMesh, 2D, polydeg=4
-  trixi_include(@__MODULE__,
-                joinpath("..", "precompile", "p4est_2d_dgsem_elixir_euler_shockcapturing_ec.jl"),
-                tspan=(0.0, 0.01),
-                initial_refinement_level=0,
-                polydeg=4,
-               )
+  include(joinpath("..", "precompile", "p4est_2d_dgsem_elixir_euler_shockcapturing_ec_polydeg4.jl"))
 
   # Compressible Euler, P4estMesh, 3D, polydeg=3
-  trixi_include(@__MODULE__,
-                joinpath("..", "precompile", "p4est_3d_dgsem_elixir_euler_source_terms_nonperiodic.jl"),
-                tspan=(0.0, 0.01),
-                initial_refinement_level=0,
-                polydeg=3,
-               )
+  include(joinpath("..", "precompile", "p4est_3d_dgsem_elixir_euler_source_terms_nonperiodic_polydeg3.jl"))
 
   # Compressible Euler, P4estMesh, 3D, polydeg=4
-  trixi_include(@__MODULE__,
-                joinpath("..", "precompile", "p4est_3d_dgsem_elixir_euler_source_terms_nonperiodic.jl"),
-                tspan=(0.0, 0.01),
-                initial_refinement_level=0,
-                polydeg=4,
-               )
+  include(joinpath("..", "precompile", "p4est_3d_dgsem_elixir_euler_source_terms_nonperiodic_polydeg4.jl"))
 
   # Compressible Euler, TreeMesh, 2D, polydeg=3
-  trixi_include(@__MODULE__,
-                joinpath("..", "precompile", "tree_2d_dgsem_elixir_euler_ec.jl"),
-                tspan=(0.0, 0.01),
-                initial_refinement_level=1,
-                polydeg=3,
-               )
+  include(joinpath("..", "precompile", "tree_2d_dgsem_elixir_euler_ec_polydeg3.jl"))
 
   # Compressible Euler, TreeMesh, 2D, polydeg=4
-  trixi_include(@__MODULE__,
-                joinpath("..", "precompile", "tree_2d_dgsem_elixir_euler_ec.jl"),
-                tspan=(0.0, 0.01),
-                initial_refinement_level=1,
-                polydeg=4,
-               )
+  include(joinpath("..", "precompile", "tree_2d_dgsem_elixir_euler_ec_polydeg4.jl"))
 
   # Compressible Euler, TreeMesh, 3D, polydeg=3
-  trixi_include(@__MODULE__,
-                joinpath("..", "precompile", "tree_3d_dgsem_elixir_euler_ec.jl"),
-                tspan=(0.0, 0.01),
-                initial_refinement_level=1,
-                polydeg=3,
-               )
+  include(joinpath("..", "precompile", "tree_3d_dgsem_elixir_euler_ec_polydeg3.jl"))
 
   # Compressible Euler, TreeMesh, 3D, polydeg=4
-  trixi_include(@__MODULE__,
-                joinpath("..", "precompile", "tree_3d_dgsem_elixir_euler_ec.jl"),
-                tspan=(0.0, 0.01),
-                initial_refinement_level=1,
-                polydeg=4,
-               )
+  include(joinpath("..", "precompile", "tree_3d_dgsem_elixir_euler_ec_polydeg4.jl"))
 end
 
 end
